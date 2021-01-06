b127b79215420e2af786a2da99eb72ee89b6f4f7
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2021-01-06 20:30:45 +0100

    Added ability to copy soldat://ip/port link

--- src/components/Lobby/ServersTable/ServerDetailsCell.tsx
+++ src/components/Lobby/ServersTable/ServerDetailsCell.tsx
@@ -1,5 +1,7 @@
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { observer } from "mobx-react";
-import React from "react";
+import React, { useRef } from "react";
 import Spinner from "src/components/Common/Spinner";
 import LobbyServersStore from "src/stores/lobby/servers"
 import { Server } from "src/types"
@@ -32,8 +34,34 @@ const ServerDetailsCell: React.FC<ServerDetailsCellProps> = props => {
         </div>
     )
 
+    const joinLink = `soldat://${props.server.ip}/${props.server.port}`;
+    const joinLinkInput = useRef(null);
+    const handleCopyLinkButtonClick = (): void => {
+        joinLinkInput.current.select();
+        document.execCommand("copy");
+    }
+
     return (
         <div className="form">
+            <div className="field">
+                <label className="label">
+                    Join link
+                </label>
+                <div className="user-input">
+                    <div className="inline">
+                        <input
+                            readOnly
+                            ref={joinLinkInput}
+                            value={joinLink}/>
+                        <div
+                            className="copy-button"
+                            onClick={handleCopyLinkButtonClick}>
+                            <FontAwesomeIcon icon={faCopy} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="field">
                 <label className="label">
                     Players
