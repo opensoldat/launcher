771f7333296a6b62e52972c7408acc0f9dec55f9
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2022-10-08 20:30:27 +0200

    Modify re-enable lobby (with temp lobby)

--- src/stores/lobby/servers.ts
+++ src/stores/lobby/servers.ts
@@ -47,7 +47,8 @@ class LobbyServersStore {
     }
 
     @action fetchPlayersList(serverIp: string, serverPort: string): Promise<void> {
-        const apiUrl = `https://api.soldat.pl/v0/server/${serverIp}/${serverPort}/players`;
+        //const apiUrl = `https://api.opensoldat.org/v0/server/${serverIp}/${serverPort}/players`;
+        const apiUrl = `http://217.182.78.135:23023/v0/server/${serverIp}/${serverPort}/players`;
         const serverId = serverIp + serverPort;
 
         this.isFetchingPlayersList[serverId] = true;
@@ -76,7 +77,8 @@ class LobbyServersStore {
     }
 
     @action fetchServers(): Promise<void> {
-        const lobbyUrl = "https://api.soldat.pl/v0/servers";
+        //const lobbyUrl = "https://api.opensoldat.org/v0/servers";
+        const lobbyUrl = "http://217.182.78.135:23023/v0/servers";
 
         function getGameMode(lobbyGameMode: LobbyGameMode): GameModes {
             switch (lobbyGameMode) {
@@ -100,10 +102,6 @@ class LobbyServersStore {
         this.servers = [];
         this.isFetching = true;
         return new Promise((resolve, reject) => {
-            this.isFetching = false;
-            reject("Lobby server has not been implemented yet");
-
-            /* TODO: reenable once lobby is ready
             fetch(lobbyUrl)
             .then(response => {
                 if (!response.ok) {
@@ -142,7 +140,7 @@ class LobbyServersStore {
                 this.servers = [];
                 console.error(error);
                 reject("An error occurred when connecting to lobby.");
-            });*/
+            });
         });
     }
 
